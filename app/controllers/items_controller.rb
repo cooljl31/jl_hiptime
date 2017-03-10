568838e15f9ef76a5279dd14123c65321e9299ba
jim lesperance <contact@jimlesperance.com>
2017-03-10 01:04:30 +0100

    work on the controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,41 @@
 class ItemsController < ApplicationController
+  before_action :find_items, onliy: [:create, :update, :edit, :destroy]
   def index
+    @items = Item.all.order('created_at DESC')
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def find_items
+    @item = Item.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit([:title, :description])
   end
 end
