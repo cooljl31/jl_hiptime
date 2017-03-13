2630a43583cd8aa129d5955bbccccca95df89818
jim lesperance <contact@jimlesperance.com>
2017-03-14 00:38:16 +0100

    and complete method and style but not finish

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :find_item, only: [:show, :update, :edit, :destroy]
+  before_action :find_item, only: [:show, :update, :edit, :destroy, :complete]
 
   def index
     @items = Item.user_login(current_user)
@@ -37,6 +37,10 @@ class ItemsController < ApplicationController
     redirect_to root_path
   end
 
+  def complete
+    @item = update_attribute(:completed_at, Time.now)
+    redirect_to root_path
+  end
   private
 
   def find_item
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :user
   scope :user_login, -> (user) { where(user_id: user).order('created_at DESC') }
+
+  def completed?
+    !completed_at.blank?
+  end
 end
